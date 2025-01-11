5a24bddf9e1696f9686546d0b1a5bc23a400480b
Adson Mettler <adsonmettler@gmail.com>
2025-01-10 21:06:57 -0500

    updated 2025-01-10

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Author: Adson Mettler do Nascimento
+        // Guessing game
+
+        // Part 1 and Part 2
+        // Console.Write("What is the magic number? ");
+        // string numberFromUser = Console.ReadLine();
+
+        // int magicNumber = int.Parse(numberFromUser);
+
+        // Part 3
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guessNumber = 0;
+
+        while (guessNumber != magicNumber)
+
+        {
+            Console.Write("What is your guess? ");
+            string guessFromUser = Console.ReadLine();
+
+            guessNumber = int.Parse(guessFromUser);
+
+            if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber == guessNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Author: Adson Mettler do Nascimento
+        // List generator and calculator
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                if (number == 0)
+                {
+                    break;
+                }
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        // Calculations
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            double average = (double)sum / numbers.Count;
+            int largest = int.MinValue;
+
+            foreach (int num in numbers)
+            {
+                if (num > largest)
+                {
+                    largest = num;
+                }
+            }
+
+            Console.WriteLine($"\nThe sum is: {sum}");
+            Console.WriteLine($"The average is: {average:F2}");
+            Console.WriteLine($"The largest number is: {largest}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
