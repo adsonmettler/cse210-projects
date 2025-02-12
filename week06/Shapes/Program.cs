7d7f4b86e5b9a1bbb464b2c864fc7e5f48963962
Beatriz Nascimento <adsonmettler@gmail.com>
2025-02-12 11:53:58 -0500

    updated 2025-Feb-12

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Yellow", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 4);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
+
+        // while ()
+        // {
+        //     Console.Write("> ");
+        //     double input = Console.ReadLine();
+        //     shapes.Add(input);
+        // }
+
+
+        Console.WriteLine($"{shapes}");
     }
 }
